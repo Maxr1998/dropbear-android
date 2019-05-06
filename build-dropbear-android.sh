44b9093c387cf239525f991116e8e3d3a7ee9d50
Maxr1998 <max.rumpf1998@gmail.com>
2019-05-06 22:46:17 +0200

    Auto install the NDK, differ between API versions

--- build-dropbear-android.sh
+++ build-dropbear-android.sh
@@ -2,55 +2,92 @@
 
 set -e
 
-if [ -z ${TOOLCHAIN} ]; then echo "TOOLCHAIN must be set. See README.md for more information."; exit -1; fi
+# Select the Android API Version to compile dropbear for, can either be 16 for Android 4.1 - 4.4, or 21 for Android 5.0+
+ANDROID_API=21
 
+# Specify the version of dropbear to build
+DROPBEAR_VERSION=2018.76
+
+# Specify binaries to build. Options: dropbear dropbearkey dropbearconvert scp dbclient
+export PROGRAMS="dropbear dropbearkey dropbearconvert"
+
+################################################################################
+
+### Setup the NDK and Toolchain
+if [ $ANDROID_API -eq 16 ]; then
+	NDK_REVISION=r10e
+elif [ $ANDROID_API -eq 21 ]; then
+	NDK_REVISION=r19c
+else
+	echo "Invalid Android API"
+	exit 1
+fi
+
+ANDROID_NDK=android-ndk-$NDK_REVISION
+if [ ! -f $ANDROID_NDK.zip ]; then
+	wget -O $ANDROID_NDK.zip https://dl.google.com/android/repository/$ANDROID_NDK-linux-x86_64.zip
+fi
+
+if [ ! -d $ANDROID_NDK ]; then
+	unzip $ANDROID_NDK.zip
+fi
+
+if [ $ANDROID_API -eq 16 ]; then
+	bash $ANDROID_NDK/build/tools/make-standalone-toolchain.sh --arch=arm --platform=android-16 --install-dir=toolchain > /dev/null 2>&1
+	export TOOLCHAIN=$PWD/toolchain
+else
+	export TOOLCHAIN=$PWD/$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64
+fi
+
+################################################################################
+
+### Setup dropbear
 # Setup the environment
 export TARGET=../target
-# Specify binaries to build. Options: dropbear dropbearkey dropbearconvert scp dbclient
-export PROGRAMS="dropbear dropbearkey dropbearconvert scp dbclient"
-# Which version of Dropbear to download for patching
-export VERSION=2018.76
 
 # Download the latest version of dropbear SSH
-if [ ! -f ./dropbear-$VERSION.tar.bz2 ]; then
-    wget -O ./dropbear-$VERSION.tar.bz2 https://matt.ucc.asn.au/dropbear/releases/dropbear-$VERSION.tar.bz2
+DROPBEAR_NAME=dropbear-$DROPBEAR_VERSION
+DROPBEAR_ARCHIVE=$DROPBEAR_NAME.tar.bz2 
+if [ ! -f $DROPBEAR_ARCHIVE ]; then
+	wget -O $DROPBEAR_ARCHIVE https://matt.ucc.asn.au/dropbear/releases/$DROPBEAR_ARCHIVE
 fi
 
 # Start each build with a fresh source copy
-rm -rf ./dropbear-$VERSION
-tar xjf dropbear-$VERSION.tar.bz2
+rm -rf $DROPBEAR_NAME
+tar xjf $DROPBEAR_ARCHIVE
 
 # Change to dropbear directory
-cd dropbear-$VERSION
+cd $DROPBEAR_NAME
 
-### START -- configure without modifications first to generate files 
-#########################################################################################################################
-echo "Generating required files..."
+################################################################################
 
+### Setup environment
 HOST=arm-linux-androideabi
-COMPILER=${TOOLCHAIN}/bin/arm-linux-androideabi-gcc
+if [ $ANDROID_API -eq 16 ]; then
+	COMPILER=${TOOLCHAIN}/bin/arm-linux-androideabi-gcc
+else
+	COMPILER=${TOOLCHAIN}/bin/armv7a-linux-androideabi21-clang
+fi
 STRIP=${TOOLCHAIN}/bin/arm-linux-androideabi-strip
 SYSROOT=${TOOLCHAIN}/sysroot
 
 export CC="$COMPILER --sysroot=$SYSROOT"
+export CFLAGS="-g -O2 -pie -fPIE -D__ANDROID_API__=$ANDROID_API" # We only support PIE compilation, and thus Android 4.1+
 
-# Android 5.0 Lollipop and greater require PIE. Default to this unless otherwise specified.
-if [ -z $DISABLE_PIE ]; then export CFLAGS="-g -O2 -pie -fPIE -D__ANDROID_API__=21"; else echo "Disabling PIE compilation..."; fi
-sleep 5
-# Use the default platform target for pie binaries 
-unset GOOGLE_PLATFORM
+unset GOOGLE_PLATFORM # Use the default platform target for pie binaries
+
+### Start compiling -- configure without modifications first to generate files
+echo "Generating required files..."
 
 # Apply the new config.guess and config.sub now so they're not patched
 cp ../config.guess ../config.sub .
-    
+
 ./configure --host=$HOST --disable-utmp --disable-wtmp --disable-utmpx --disable-zlib --disable-syslog > /dev/null 2>&1
 
 echo "Done generating files"
 sleep 2
 echo
 echo
-#########################################################################################################################
-### END -- configure without modifications first to generate files 
 
 # Begin applying changes to make Android compatible
 # Apply the compatibility patch
@@ -62,21 +99,21 @@ cd -
 
 echo "Compiling for ARM"  
 
-cd dropbear-$VERSION
-    
+cd $DROPBEAR_NAME
+
 ./configure --host=$HOST --disable-utmp --disable-wtmp --disable-utmpx --disable-zlib --disable-syslog
 
-make PROGRAMS="$PROGRAMS"
+make PROGRAMS="$PROGRAMS" -j $(nproc)
 MAKE_SUCCESS=$?
 if [ $MAKE_SUCCESS -eq 0 ]; then
 	clear
 	sleep 1
-  	# Create the output directory
+	# Create the output directory
 	mkdir -p $TARGET/arm;
 	for PROGRAM in $PROGRAMS; do
 
 		if [ ! -f $PROGRAM ]; then
-    		echo "${PROGRAM} not found!"
+			echo "${PROGRAM} not found!"
 		fi
 
 		$STRIP "./${PROGRAM}"
@@ -85,6 +122,6 @@ if [ $MAKE_SUCCESS -eq 0 ]; then
 	cp $PROGRAMS $TARGET/arm
 	echo "Compilation successful. Output files are located in: ${TARGET}/arm"
 else
- 	echo "Compilation failed."
+	echo "Compilation failed."
 fi
 
